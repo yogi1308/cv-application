d80426b460c4e2b5296c2fdcc8ee4c53b851756e
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-06-27 15:02:38 -0700

    refactor: enhance sidebar and customize component styles for better responsiveness

--- src/components/sidebar/customize.jsx
+++ src/components/sidebar/customize.jsx
@@ -2,10 +2,12 @@ function Customize() {
     return (
         <div className="customize-container">
             <label htmlFor="font">Select Font</label>
-            <select name="font" id="">
-                <option value="">Comic Sans</option>
-                <option>Times New Roman</option>
-            </select>
+            <div className="custom-select">
+                <select name="font" id="">
+                    <option value="">Comic Sans</option>
+                    <option>Times New Roman</option>
+                </select>
+            </div>
         </div>
     )
 }
